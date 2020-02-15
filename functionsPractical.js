57209999b0f1fc9b0a70d550d11fb240c29cacc1
Jordan Walker <jwalker117@gmail.com>
2020-02-15 15:34:38 -0600

    Adding W4 Video 2 Notes

---
+++ functionsPractical.js
@@ -0,0 +1,27 @@
+/*
+	Functions Practical
+
+*/
+
+//basic function
+function logger() {
+	console.log('in logger functions');
+}
+
+//function with return
+function letter() {
+	console('in letter function');
+	return 'hello world!';
+}
+
+//math function example
+function mathExample () {
+	console.log('in mathExample function');
+	return 7 * 9;
+}
+
+//math function example that except parameters
+function mathAgain(a , b) {
+	console.log('in mathAgain function a and b are:', a, b);
+	return a + b;
+}
