040ad4bb2e5cd1b42d03a21284bdbde7889e9c5d
Jordan Walker <jwalker117@gmail.com>
2020-02-15 15:16:05 -0600

    Adding W4 Video 1 Notes

---
+++ functionsConceptual.js
@@ -0,0 +1,42 @@
+/*
+	Functions Conceptual
+
+A funciton is a block of code designed to preform a taks.
+A funciton is executed when "something" invokes it(call it).
+
+Function parameters are list inside the () in the function definition.
+Function arguments are teh values received by the function when it is invoked.
+
+Function Syntax
+function name(parameter1, parameter2, parameter3) {
+  // code to be executed
+}
+
+pseudo-code for a function
+
+function fillMugWithCoffee {
+	place mug under coffee maker
+	press handle
+	hold until full
+}
+
+function fillMugWith(drink) {
+	place mug in under spout
+	use drink's receptical to fill mug
+}
+
+function fillMugWith(ingredient1, ingredient2, ingredient3) {
+	fill mug1/3 full w/ ingredient1
+	fill mug1/3 full w/ ingredient2
+	fill mug1/3 full w/ ingredient3
+}
+*/
+
+
+
+
+
+
+
+
+
