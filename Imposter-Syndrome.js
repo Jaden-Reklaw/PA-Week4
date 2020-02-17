4b47fee51adeee2b19f6ca5bb4a189d49a9314b2
Jordan Walker <jwalker117@gmail.com>
2020-02-16 18:39:30 -0600

    Adding W4 Video 4 Notes

---
+++ Imposter-Syndrome.js
@@ -0,0 +1,33 @@
+/*
+	Imposter Syndrome
+
+	What is imposter syndrome?
+	Can be defined as a collection of feelings of inadequacy that 
+	persist despite evident success. 'Imposters' suffer from chronic
+	self-doubt and a sense of intellectual fraudulence that override 
+	any feelings of success or external proof of their competence.
+
+	Computer Science and User Experience are broad fields. They are
+	tough for any newbie to find success.
+
+	- New knowledge & continual learning.
+	- Cumulative Skills Enhancements
+
+	Imposter Syndrome can be countered
+	If not it can be demotivating.
+	Don't hide what you don't know.
+
+	How does it happen?
+	When things get hard and others seem to be reaching higher points
+	while you fall short of the goals. Avoid comparisons with others.
+
+	Measure yourself against yourself. Self-evaluate kindly but honestly.
+	Mine your mistakes. Dig into your mistakes in order to truely understand
+	what your missing.
+
+	Throw away your guilt. Guilt leads to stress and distractions.
+
+	Create good habits.
+
+	Engage with others that are beneficial for your growth.
+*/
